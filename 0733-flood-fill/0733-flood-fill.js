6e34c6b2f730f2133ba3136f1398cc0c611dcc93
Tufahel Ahmed <tufahel96@gmail.com>
2022-10-18 21:40:48 +0600

    Time: 141 ms (17.57%), Space: 43.7 MB (93.23%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, color) {
+    if(image[sr][sc] == color) return image;
+    fill(image, sr, sc, image[sr][sc], color);
+    return image;
+};
+
+function fill(image, sr, sc, color, newColor){
+    if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || image[sr][sc] !== color )
+        return;
+    image[sr][sc] = newColor;
+    fill(image, sr-1, sc, color, newColor);
+    fill(image, sr+1, sc, color, newColor);
+    fill(image, sr, sc-1, color, newColor);
+    fill(image, sr, sc+1, color, newColor);
+}
